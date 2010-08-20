fa06dc9790916d65c082fb9fa51a829218e4d9ed
Shane Emmons <semmons99@gmail.com>
2010-08-20 08:46:03 -0400

    document lib/go.rb

--- lib/go.rb
+++ lib/go.rb
@@ -1,6 +1,16 @@
 require 'go/game'
 
 module Go
+  ##
+  # Creates a new +Go::Game+ using the specified +board_size+.
+  #
+  # @param [Integer] board_size The size of the Go board. Standard sizes are
+  #   9x9, 13x13 and 19x19.
+  #
+  # @return [Go::Game] The new +Go::Game+.
+  #
+  # @example
+  #   game = Go.new_game(13) #=> <Go::Game...>
   def self.new_game(board_size=19)
     Go::Game.new(board_size)
   end
