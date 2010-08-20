af24d22291cb1de01e384745a3b20ce389558a84
Shane Emmons <semmons99@gmail.com>
2010-08-20 09:29:11 -0400

    document lib/go/game.rb

--- lib/go/game.rb
+++ lib/go/game.rb
@@ -1,30 +1,62 @@
 require 'go/board'
 
 module Go
+  ##
+  # Stores the state of a game of Go and provided methods for players to
+  # interact with the board.
   class Game
+    ##
+    # @return [Symbol, :black, :white] The color of the current players turn.
     attr_reader :turn
 
+    ##
+    # Starts a new game of Go.
+    #
+    # @param [Integer] board_size The size of the Go board. Standard sizes are
+    #   9x9, 13x13 and 19x19.
+    #
+    # @return [Go::Game] +self+
     def initialize(board_size=19)
       @turn  = :white
       @board = Board.new(board_size)
     end
 
+    ##
+    # Returns the current state of the board.
+    #
+    # @return [Array[Array]] Current state of the board.
     def board
       @board.layout
     end
 
+    ##
+    # Passes the current players turn.
+    #
+    # @return [Go::Game] +self+
     def pass
       @board.sync_previous_layout
       next_turn
       self
     end
 
+    ##
+    # Places a stone at the specified +row+/+col+ for the current player.
+    #
+    # @param [Integer] row The row to place the stone in.
+    # @param [Integer] col The column to place the stone in.
+    #
+    # @return [Go::Game] +self+
     def place_stone(row, col)
       @board.place_stone(row, col, @turn)
       next_turn
       self
     end
     
+    ##
+    # Returns a string representation of the board which can be output to the
+    # console.
+    #
+    # @return [String] Pretty version of the board for use in console output.
     def pretty_board
       @board.layout.map{|row|
         row.map{|cell|
@@ -42,6 +74,10 @@ module Go
 
     private
 
+    ##
+    # Updates the current players turn to the next
+    #
+    # @return [Symbol, :black, :white] The color of the new turns player.
     def next_turn
       @turn = @turn == :white ? :black : :white
     end
